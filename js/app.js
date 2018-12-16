2b9e2c7aacc1fb3736fc72c24dfc4f44a0f092d5
msaladyk <maciej.saladyk@gmail.com>
2018-12-16 12:38:39 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,95 @@
+// Add button
+var addTaskBtn = document.querySelector('#addTaskButton');
+
+// Task input
+var taskInput = document.querySelector('#taskInput');
+
+// Error
+var error = document.querySelector('#errorPlaceholder');
+console.log(error);
+
+// Task List
+var taskList = document.querySelector('#taskList');
+
+//Complete button
+// var completeButtons = document.querySelectorAll('button.completeBtn');
+
+// Counter
+var counter = document.querySelector('#counter span');
+counter.innerText = 0;
+
+// Add Task Event
+addTaskBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (taskInput.value.length > 4 && taskInput.value.length < 101) {
+
+        var newTask = document.createElement('li');
+        newTask.innerHTML = '<button></button><div><h1></h1></div><button></button>';
+        newTask.style.listStyleType = 'none';
+        newTask.style.borderBottom = '2px solid black';
+
+
+        newTask.firstElementChild.className = 'complete';
+        newTask.firstElementChild.display = 'inline';
+
+        newTask.firstElementChild.nextElementSibling.className = 'task';
+        newTask.firstElementChild.nextElementSibling.innerText = taskInput.value;
+        newTask.firstElementChild.nextElementSibling.style.margin = '0 30px 0 10px';
+
+        newTask.lastElementChild.className = 'deleteBtn';
+        newTask.lastElementChild.display = 'inline-block';
+
+        var hook = document.querySelector('#taskList');
+        counter.innerText++;
+
+        hook.appendChild(newTask);
+        taskInput.value = '';
+
+        error.innerText = '';
+
+    } else {
+        error.style.color = '#ed2553';
+        error.style.fontWeight = '600';
+        error.innerText = 'Task must contain at least 5 and no more than 100 characters';
+    }
+});
+
+
+// Complete / incomplete
+
+taskList.addEventListener("click", function (e) {
+    if (e.target.className === 'complete' && e.target.parentElement.className === "") {
+        e.target.parentElement.classList.toggle('completed');
+        counter.innerText--;
+        e.target.style.backgroundImage = "url('images/correct-mark.png')";
+        e.target.style.backgroundSize = 'contain';
+        e.target.style.backgroundOrigin = 'content-box';
+        e.target.style.backgroundRepeat = 'no-repeat';
+        e.target.style.backgroundPosition = 'center';
+    } else if (e.target.className === 'complete' && e.target.parentElement.className === "completed") {
+        e.target.parentElement.classList.toggle('completed');
+        counter.innerText++;
+        e.target.style.background = 'none';
+    }
+});
+// Delete task button
+
+taskList.addEventListener('click', function(event) {
+   if(event.target.className === 'deleteBtn') {
+       event.target.parentElement.remove();
+       if (event.target.parentElement.className !== 'completed') {
+           counter.innerText--;
+       }
+   }
+});
+
+// Remove all
+var rmvBtn = document.querySelector('#removeFinishedTasksButton');
+
+rmvBtn.addEventListener('click', function() {
+    var lis = document.querySelectorAll('#taskList li.completed');
+    for (var i = 0; i < lis.length; i++) {
+        lis[i].remove();
+    }
+});
